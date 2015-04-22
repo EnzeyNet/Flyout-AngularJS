5b2784b8a78f96022ac0c0fa812e12fc8a495363
Joseph Aspelund <jaspelund@ptc.com>
2015-04-22 15:42:17 -0500

    Initial Commit

---
+++ dist/Flyout-AngularJS.js
@@ -0,0 +1,37 @@
+(function (angular) {
+    "use strict";
+
+	var module = angular.module('net.enzey.flyout', []);
+	var repositionEventTrigger = 'NZ_EVENT_REPOSITION';
+
+	module.directive('nzFlyout', ['$compile', function ($compile) {
+		return {
+			priority: 9999,
+			compile: function ($element, $attrs) {
+				angular.extend($element[0], EnzeyNet.FlyoutServices.prototype);
+				$element[0].createdCallback();
+
+				return {
+					pre: function(scope, element, attrs) {
+						var displayFlyout = element[0].displayFlyout;
+						var lastFlyoutContainer = null;
+						element[0].displayFlyout = function() {
+							displayFlyout.call(this, arguments);
+							if (lastFlyoutContainer != element[0].flyoutContainer) {
+								lastFlyoutContainer = element[0].flyoutContainer;
+								$compile(lastFlyoutContainer)(scope);
+								scope.$apply();
+							}
+						}
+					},
+					post: function(scope, element, attrs) {
+						element[0].attachedCallback();
+					}
+				}
+
+			}
+		};
+
+	}]);
+
+})(angular);
